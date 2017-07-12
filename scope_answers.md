5f4b4f140fccfe793a8f4fa711183a4b93d53432
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-12 14:58:40 -0400

    exercise 1, completed until 7

---
+++ scope_answers.md
@@ -0,0 +1,15 @@
+<!-- Exercise 1 -->
+
+1. Outputs 'welcome' because lastWord was defined as 'welcome' before the console.log
+
+2. Outputs 'up here!' because the message variable runs through the function shout()
+
+3.
+
+4. Outputs 2 dozen; var muffins and purchasedMuffins are global and get run thru the getMuffins function
+
+5. Output is sneak out, because var chore = sneak out is defined within the function and that's the function being run.
+
+6. Outputs 'looking for gold' because getMail function is run and var contents = 'looking for gold' is a global variable. var contents = 'struck it rich' will only output if changeContents() is run.
+
+7.
---
+++ test.js
@@ -0,0 +1,58 @@
+// var lastWord = 'welcome';
+// console.log(lastWord);
+// lastWord = 'goodbye';
+//
+// var message = "Up here!";
+//
+// function shout() {
+//   console.log(message);
+// }
+//
+// shout();
+
+var message = "Up here!";
+
+function shout(message) {
+  console.log(message);
+}
+
+shout("Down below!")
+//
+// var muffins = 'two dozen';
+// var purchasedMuffins;
+//
+// function getMuffins() {
+//   return muffins;
+// }
+// purchasedMuffins = getMuffins();
+// console.log(purchasedMuffins);
+//
+// var chore = 'laundry';
+//
+// function doChores() {
+//   var chore = 'sneak out';
+//
+//   function reportActivity() {
+//     console.log(chore);
+//   }
+//
+//   reportActivity();
+// }
+//
+// doChores(); // calling doChores(), which then calls reportActivity()
+
+var letter;
+var contents = 'Looking for gold';
+
+function getMail() {
+
+  function changeContents() {
+    var contents = 'Struck it rich!';
+  }
+
+  changeContents();
+  return contents;
+}
+
+letter = getMail();
+console.log(letter);
